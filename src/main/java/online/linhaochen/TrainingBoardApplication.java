e1c6dd1949e1b044c548353bc2b61f134ac46a9d
林浩琛 <15904242+linhaochen666@user.noreply.gitee.com>
2026-06-25 11:45:53 +0800

    项目功能全部实现的第一个发行版。

---
+++ src/main/java/online/linhaochen/TrainingBoardApplication.java
@@ -0,0 +1,30 @@
+package online.linhaochen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+public class TrainingBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingBoardApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
--- src/test/java/online/linhaochen/trainingboard/TrainingBoardApplicationTests.java
+++ src/test/java/online/linhaochen/trainingboard/TrainingBoardApplicationTests.java
@@ -2,10 +2,10 @@ package online.linhaochen.trainingboard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = {
-        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration"
-})
+@SpringBootTest
+@ActiveProfiles("test")
 class TrainingBoardApplicationTests {
 
     @Test
